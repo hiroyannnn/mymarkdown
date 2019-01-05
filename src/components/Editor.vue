e1f15fc5fe346b4f87680401a891a2e81d3d9d8c
hiro <hiro@hironoMacBook-puro.local>
2019-01-05 21:26:07 +0900

    5.4 :tada: save memo  to DB

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -10,6 +10,7 @@
                 </div>
                 <button class="addMemoBtn" @click="addMemo">メモの追加</button>
                 <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
+                <button class="saveMemoBtn" @click="saveMemo">メモの保存</button>
             </div>
             <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
             <div class="preview" v-html="preview()"></div>
@@ -45,6 +46,12 @@ export default {
             if(this.selectedIndex > 0){
                 this.selectedIndex--;
             }
+        },
+        saveMemo: function(){
+            firebase
+                .database()
+                .ref("memos/" + this.user.uid)
+                .set(this.memos);
         },
         selectMemo: function(index){
             this.selectedIndex = index;
@@ -86,6 +93,9 @@ export default {
 }
 .addMemoBtn{
     margin-top:  20px;
+}
+.deleteMemoBtn{
+    margin: 10px;
 }
 .markdown{
     width: 40%;
